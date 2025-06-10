09abbab0bbe6d6d88eec2aa430bbef11c85f9d92
Bruno <bd687353@gmail.com>
2025-06-10 16:09:24 -0300

    Aplicando desconto nos livros de 30%

--- app/main.js
+++ app/main.js
@@ -1,12 +1,15 @@
 import insertBook from "./methodForEarch.js";
-let book = [];
-const endpointAPI ="https://guilhermeonrails.github.io/casadocodigo/livros.json";
+import ApplycDiscount from "./methodMap.js";
+let books = [];
+const endpointAPI =
+  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 console.log;
 
 async function getSeachBooks() {
   const res = await fetch(endpointAPI);
-  book = await res.json();
-  insertBook(book);
+  books = await res.json();
+  let discountBooks = ApplycDiscount(books);
+  insertBook(discountBooks);
 }
 
 getSeachBooks();
