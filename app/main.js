d7d7a3f8f900072b2867ca8cda72cb6bbc563c51
Bruno <bd687353@gmail.com>
2025-06-10 17:54:58 -0300

    -Filtar as categorias dos livros

--- app/main.js
+++ app/main.js
@@ -1,9 +1,31 @@
 import insertBook from "./methodForEarch.js";
 import ApplycDiscount from "./methodMap.js";
+import filterBooks from "./methodFilter.js";
+
 let books = [];
+const btnfilterBooks = document.querySelectorAll(".nav button");
+
 const endpointAPI =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
-console.log;
+
+// filtear livra nas categorias usando o chamado da função filterBooks
+
+btnfilterBooks.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const category = event.target.value;
+
+    const filteredBooks = filterBooks(books, category);
+
+    insertBook(filteredBooks);
+  });
+});
+
+// btnfilterBooksofFront.addEventListener("click", () => {
+//   const filteredBooks = filterBooks(books);
+//   console.table(filteredBooks);
+//   insertBook(filteredBooks);
+// });
 
 async function getSeachBooks() {
   const res = await fetch(endpointAPI);
